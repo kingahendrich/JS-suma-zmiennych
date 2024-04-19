39083dc94c8e114ac817d6da186a3cc7006c70ed
KingaH9711 <ingainmainz@gmail.com>
2024-04-19 11:02:05 +0200

    gotowy

---
+++ main.js
@@ -0,0 +1,9 @@
+let firstNumber = 67;
+let secondNumber = 28;
+
+function sumaZmiennych (num1,num2) {
+    let sum = num1 + num2; 
+    return sum;
+}
+
+// console.log(sumaZmiennych(firstNumber,secondNumber));
